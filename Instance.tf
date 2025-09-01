6e07e11d6cfc41c05404e423ad7f8f1ccef0b042
Tejesh1105 <154406957+Tejesh1105@users.noreply.github.com>
2025-09-01 21:41:49 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,16 +1,38 @@
-resource "aws_instance" "terraformweb" {
+resource "aws_instance" "web" {
   ami                    = var.amiID[var.region]
   instance_type          = "t3.micro"
   key_name               = "terra-key"
-  vpc_security_group_ids = [aws_security_group.terra-sg.id]
+  vpc_security_group_ids = [aws_security_group.dove-sg.id]
   availability_zone      = var.zone1
 
   tags = {
-    Name = "Terra-web"
+    Name = "Dove-web"
     Name = "Terra-Project"
   }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.webuser
+    private_key = file("terrakey")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
 }
+
 resource "aws_ec2_instance_state" "web-state" {
   instance_id = aws_instance.web.id
   state       = "running"
 }
+
+
